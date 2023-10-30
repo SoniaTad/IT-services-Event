c313628d3ce0569d28bba910bb2ab7cae5228ed5
SoniaTad <92749781+SoniaTad@users.noreply.github.com>
2023-10-30 11:47:22 +0000

    Created staff.sql

---
+++ db/staff.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE staff;
+use staff;
+
+CREATE TABLE Staff_details (
+  ID int AUTO_INCREMENT ,
+  Name VARCHAR(255) NOT NULL ,
+  Email VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (ID)
+  
+);
